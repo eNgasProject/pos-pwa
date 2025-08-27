df2843261faad610fb7a32a14dca786f31fc2a90
eNgasProject <pinzinlimited@gmail.com>
2025-08-27 20:27:11 +0000

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAm8dDV1HEXDKcsoj535xbo0FqjM3Q0x7g",
-  authDomain: "posapp-afd6f.firebaseapp.com",
-  projectId: "posapp-afd6f",
-  storageBucket: "posapp-afd6f.firebasestorage.app",
-  messagingSenderId: "742610729359",
-  appId: "1:742610729359:web:896f44fabc9c9544a473fa",
-  measurementId: "G-CLZ1XYK53V"
+  
 };
 
 const app = initializeApp(firebaseConfig);
